6f95ce3a36ab66d0156d70125ca429fa8631f8fd
Aman Kumar <97603917+JARHEADXD@users.noreply.github.com>
2023-03-05 19:54:36 +0530

    Add files via upload

---
+++ newscript.js
@@ -0,0 +1,5 @@
+searchBar = document.getElementById("search-icon")
+searchArea = document.querySelector(".search-bar")
+searchBar.addEventListener("click", ()=>{
+    searchArea.classList.toggle("search-bar")
+})
